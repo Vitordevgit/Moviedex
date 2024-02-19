f7b087f46380c3802276ad7eba62b2bc06d7b37f
Vitor de Oliveira <vitoro.fabiano@gmail.com>
2024-02-18 23:47:57 -0300

    Update home page content

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Home...</h1>
+    <div className="text-primary flex flex-col items-center pt-5">
+      <div className="">
+        <p className="text-2xl font-light">Liste seus filmes favoritos.</p>
+        <p className="text-2xl font-light">
+          Crie uma lista de filmes para assistir.
+        </p>
+        <p className="text-2xl font-light">Compartilhe seas analises.</p>
+        <p className="text-2xl font-bold text-secondary">
+          Tudo isso sem anúncios.
+        </p>
+      </div>
     </div>
   );
 }
