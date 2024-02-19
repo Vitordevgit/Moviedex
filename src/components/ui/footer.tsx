bb1e95bec5e3ee09cfd950d619770612e18432ab
Vitor de Oliveira <vitoro.fabiano@gmail.com>
2024-02-18 23:47:17 -0300

    create and add footer component on layout

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="mt-8 px-8 py-4 text-[0.725rem] opacity-75 text-primary">
+      © 2023 Copyright <span className="font-semibold">MovieDEX</span>
+    </div>
+  );
+};
+
+export default Footer;
