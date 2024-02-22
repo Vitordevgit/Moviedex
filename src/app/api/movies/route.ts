23b0fa1738c072c766bf23c6712eee26bfea7737
Vitor de Oliveira <vitoro.fabiano@gmail.com>
2024-02-21 23:10:14 -0300

    refactor: movie API route to handle errors and improve parameter handling

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,9 +1,33 @@
 const GET = async () => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&language=pt-BR`
-  );
-  const data = await response.json();
-  return Response.json(data);
+  try {
+    // Calcular a data de um ano atrás
+    const oneYearAgo = new Date();
+    oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+    const formattedDate = oneYearAgo.toISOString().split("T")[0];
+
+    // Parâmetros da API
+    const apiKey = process.env.TMDB_API_KEY;
+    const language = "pt-BR";
+    const sortBy = "popularity.desc"; // Ordenar por popularidade descendente
+    const releaseDateGte = formattedDate; // Data de lançamento maior ou igual a um ano atrás
+
+    // Montar a URL com os parâmetros
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=${language}&sort_by=${sortBy}&release_date.gte=${releaseDateGte}`;
+
+    // Realizar a requisição
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Erro ao buscar dados");
+    }
+
+    const data = await response.json();
+    return new Response(JSON.stringify(data), { status: 200 });
+  } catch (error) {
+    console.error("Erro na solicitação:", error);
+    return new Response(JSON.stringify({ error: "Erro na solicitação" }), {
+      status: 500,
+    });
+  }
 };
 
 export { GET };
